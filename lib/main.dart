32141e71956acbdef1c922cc717e11e25af6707a
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-01 01:16:04 -0300

    Adição de lógica inicial do body da aplicação.
    Consiste de um FutureBuilder que vai fazer a solicitação assíncrona para a api e construir o corpo da aplicação de acordo com a resposta.

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,15 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-const request = "https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,currencies&key=3014b5b6";
+const request =
+    "https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,currencies&key=3014b5b6";
 
-void main() async{
+void main() async {
   //print(await getData());
   runApp(MeuApp());
 }
 
-Future<Map> getData() async {
+Future<Map> _getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
 }
@@ -33,9 +34,47 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurple,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+        title: Text("\$ Conversor \$", textAlign: TextAlign.center),
+      ),
+      body: FutureBuilder<Map>(
+        future: _getData(),
+        builder: (context, snapshot) => _buildResponse(context, snapshot),
+      ),
     );
   }
 }
 
+Widget _buildResponse(context, snapshot) {
+  switch (snapshot.connectionState) {
+    case ConnectionState.none:
+    case ConnectionState.waiting:
+      return _buildText("Carregando, aguarde...");
+      break;
+    default:
+      if (snapshot.hasError) {
+        return _buildText("Erro ao carregar dados");
+      } else {
+        return _buildBody();
+      }
+      break;
+  }
+}
+
+Widget _buildText(String text) {
+  return Center(
+    child: Text(
+      text,
+      style: TextStyle(color: Colors.amber, fontSize: 30),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Widget _buildBody() {
+  return Container();
+}
