e7cbcd3c3bc0d69eee3d7e20373fe0db7335f272
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-04-01 16:11:10 -0300

    Aplicação de temas e construção dos TextFields.

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,16 @@ class MeuApp extends StatelessWidget {
     return MaterialApp(
       title: "conversor_de_moedas",
       home: MyHomePage(),
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            hintStyle: TextStyle(color: Colors.amber),
+          )),
     );
   }
 }
@@ -32,6 +42,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _dolar = 0;
+  double _euro = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,34 +60,77 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
 
-Widget _buildResponse(context, snapshot) {
-  switch (snapshot.connectionState) {
-    case ConnectionState.none:
-    case ConnectionState.waiting:
-      return _buildText("Carregando, aguarde...");
-      break;
-    default:
-      if (snapshot.hasError) {
-        return _buildText("Erro ao carregar dados");
-      } else {
-        return _buildBody();
-      }
-      break;
+  Widget _buildResponse(context, snapshot) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.none:
+      case ConnectionState.waiting:
+        return _buildText("Carregando, aguarde...");
+        break;
+      default:
+        if (snapshot.hasError) {
+          return _buildText("Erro ao carregar dados");
+        } else {
+          print(snapshot.data["currencies"]);
+          _refreshResults(snapshot.data["currencies"]);
+          return _buildBody();
+        }
+        break;
+    }
   }
-}
 
-Widget _buildText(String text) {
-  return Center(
-    child: Text(
-      text,
-      style: TextStyle(color: Colors.amber, fontSize: 30),
-      textAlign: TextAlign.center,
-    ),
-  );
-}
+  Widget _buildText(String text) {
+    return Center(
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.amber, fontSize: 30),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
 
-Widget _buildBody() {
-  return Container();
+  void _refreshResults(Map data) {
+    _dolar = data["USD"]["buy"];
+    _euro = data["EUR"]["buy"];
+  }
+
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Icon(
+              Icons.monetization_on,
+              color: Colors.amber,
+              size: 130.0,
+            ),
+          ),
+          _gerarCampoTexto("Reais", "R\$"),
+          Divider(),
+          _gerarCampoTexto("Dólares", "US\$"),
+          Divider(),
+          _gerarCampoTexto("Euros", "€"),
+          Divider(),
+          _gerarCampoTexto("Libras", "£"),
+          Divider(),
+          _gerarCampoTexto("Bitcoins", "₿")
+        ],
+      ),
+    );
+  }
+
+  Widget _gerarCampoTexto(String labelText, String prefixText) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          labelText: (labelText),
+          labelStyle: TextStyle(color: Colors.amber, fontSize: 25),
+          border: OutlineInputBorder(),
+          prefixText: prefixText),
+      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    );
+  }
 }
